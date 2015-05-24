de4bae563a5be80305c9f93f65a7c12894541843
jiyigrace <jiyigrace@Yis-iMac.local>
2015-05-24 16:29:19 -0400

    Assignment 2 for R programming
    I updated my codes for two functions assigned by coursera R programming
    course.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The first function, makeCacheMatrix creates a special "vector", which is really a list containing a function to
+#set the value of the vector
+#get the value of the vector
+#set the value of the inverse
+#get the value of the inverse
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix<- function(x = matrix()) {
+        inv<- NULL
+        set <- function(y) {
+                x <<- y
+                inv<<- NULL
+        }
+        get <- function() x
+        setinv <- function(inverse) inv <<- inverse
+        getinv <- function()inv
+        list(set = set, get = get,
+             setinv = setinv,
+             getinv = getinv)
 }
 
-
-## Write a short comment describing this function
-
+#The following function calculates the inverse of the special "vector" created with the above function.
+#However, it first checks to see if the inverse has already been calculated. 
+#If so, it gets the inverse from the cache and skips the computation.
+#Otherwise, it calculates the inverse of the data
+# and sets the value of the inverse in the cache via the setinv function.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        inv <- x$getinv()
+        if(!is.null(inv)) {
+                message("getting cached inverse matrix")
+                return(inv)
+        }else{
+        inv_x <- solve(x$get())
+        x$setinv(inv_x)
+        return(inv_x)
+}
 }
